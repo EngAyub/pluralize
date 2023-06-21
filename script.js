9b34fac752234a01128ef1a72932847cabd87913
ENG AYUUB <moztaphem9@gmal.com>
2023-06-21 20:41:15 +0300

    Pluralize done3

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Good Luck 💪🏾
+function pluralize(Arr) {
+  var wordCount = {};
+  var pluralArr = [];
+
+  for (var i = 0; i < Arr.length; i++) {
+    var word = Arr[i];
+    if (wordCount[word]) {
+      var pluralForm = word + 's';
+      if (!wordCount[pluralForm]) {
+        var singularIndex = pluralArr.indexOf(word);
+        pluralArr[singularIndex] = pluralForm; 
+        wordCount[pluralForm] = true;
+      }
+    } else {
+      wordCount[word] = true;
+      pluralArr.push(word); 
+    }
+  }
+  return pluralArr;
+}
+var Arr = (["cat", "dog", "cat", "mouse"]);  // cats
+var pluralArr = pluralize(Arr);
+
+console.log(pluralArr);
